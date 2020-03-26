59114ee4e3a3691e1fa58b087c4a7199d660082e
Windmill-City <1449182174@qq.com>
2020-03-26 11:56:20 +0800

    添加项目文件。

---
+++ C_P56d/C_P56d.cpp
@@ -0,0 +1,30 @@
+﻿// C_P56d.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    int Days;
+    printf("Input how many days you've lated to return the book:\n");
+    scanf_s("%d", &Days);
+    if (Days <= 5)
+        printf("You should pay 50 paise.");
+    else if (Days <= 10)
+        printf("You sholud pay 1 rupee.");
+    else if (Days <= 30)
+        printf("You should pay 5 rupee.");
+    else
+        printf("Your memership will be cancelled.");
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
